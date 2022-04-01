6d6002cbff783c2f216fe14c8bf4ea3585b9c0ae
OliverMengich <kemeioliver2000@gmail.com>
2022-04-01 13:58:16 +0300

    3 phase controll worked

---
+++ 3_phase_speed_meter.ino
@@ -0,0 +1,68 @@
+int delayTime = 0;
+int sensor_pot=A0;
+int sensor_value=0;
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(sensor_pot,INPUT);
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor_value = analogRead(sensor_pot);
+  delayTime = map(sensor_value,0,1023,0,255);
+  lcd.setCursor(0,1);
+  lcd.clear();
+  lcd.print(delayTime);
+  lcd.print("%");
+  //1 5 6
+  digitalWrite(1,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+   digitalWrite(4,LOW);
+  delay(delayTime);
+  //1,2,6
+  digitalWrite(1,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(5, LOW);
+  delay(delayTime);
+  //1 ,2 ,3
+  digitalWrite(1,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(6,LOW);
+  delay(delayTime);
+  //2, 3 4
+  digitalWrite(4,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+  delay(delayTime);
+  // 3, 4,5
+  digitalWrite(4,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(2,LOW);
+  delay(delayTime);
+  // 4 ,5 6
+  digitalWrite(4,HIGH);
+   digitalWrite(6,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(3,LOW);
+  delay(delayTime);
+}
